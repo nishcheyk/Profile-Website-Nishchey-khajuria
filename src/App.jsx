88504c5bf9ddb8ba130f9477feb683f17216d7b6
Nishchey khajuria <nishcheycapture2014@gmail.com>
2025-06-17 16:15:40 +0530

    ststic .json added

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,23 @@ import Contact from './components/Contact';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 
+// App.js (cleaned)
 const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar/>
+    <>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/experience' element={<Experience/>}/>
-        <Route path='/projects' element={<Projects/>}/>
-        <Route path='/skills' element={<Skills/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/contact' element={<Contact/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-      <Footer/>
-    </BrowserRouter>
+      <Footer />
+    </>
   );
-}
+};
+
 
 export default App;
