da74df348edede112c0e7e55edc746e4bb9139d8
Nishchey khajuria <nishcheycapture2014@gmail.com>
2025-06-17 16:08:35 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import Experience from './components/Experience';
+import About from './components/About';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
 
 const App = () => {
   return (
@@ -11,6 +15,10 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/experience' element={<Experience/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/skills' element={<Skills/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
       </Routes>
       <Footer/>
     </BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -3,26 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Experience from './components/Experience';
-import About from './components/About';
-import Contact from './components/Contact';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
+import App from './App.jsx';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/experience' element={<Experience/>}/>
-        <Route path='/projects' element={<Projects/>}/>
-        <Route path='/skills' element={<Skills/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-      </Routes>
+      <App/>
     </BrowserRouter>
 );
 
